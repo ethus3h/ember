85afa291783bcf7f4e32ea17ade02de844b18d78
ethus3h <kolubat@gmail.com>
2016-08-29 12:40:21 -0400

    Web site update

--- ember-satellite-projects/futuramerlin-website/source/assets/news/wp-content/themes/fm-wp-theme/header.php
+++ ember-satellite-projects/futuramerlin-website/source/assets/news/wp-content/themes/fm-wp-theme/header.php
@@ -24,24 +24,29 @@
 			"/">ember</a>
 		</p>
 		<ul>
-			<li>Navigation:</li>
+            <li>Navigation:</li>
 			<li class="nav-item nav-item-inactive index">
 				<a href="/">Home</a>
 			</li>
-			<li class="nav-item nav-item-inactive bio">
-				<a href="/blog">Blog</a>
+			<li class="nav-item nav-item-active news">
+				<a href="/news">News</a>
 			</li>
-			<li class="nav-item nav-item-inactive news">
-				<a href="/specification">Specification</a>
+            <li class="nav-item nav-item-inactive ready-to-use">
+				<a href="/ancillary/ready-to-use.htm">Ready-to-use software</a>
+			</li>			<li class="nav-item nav-item-inactive specification">
+				<a href="/specification">Core: Specification</a>
 			</li>
-			<li class="nav-item nav-item-inactive blog">
-				<a href="/community">Community</a>
+			<li class="nav-item nav-item-inactive community">
+				<a href="/community">Core: Community</a>
 			</li>
-			<li class="nav-item nav-item-inactive contact">
-				<a href="/computing">Computing</a>
+			<li class="nav-item nav-item-inactive computing">
+				<a href="/computing">Core: Computing</a>
 			</li>
-			<li class="nav-item nav-item-inactive resume">
-				<a href="/library">Library</a>
+			<li class="nav-item nav-item-inactive library">
+				<a href="/library">Core: Library</a>
+			</li>
+            <li class="nav-item nav-item-inactive work-in-progress">
+				<a href="/ancillary/work-in-progress.htm">Other work in progress</a>
 			</li>
 			<li class="nav-item nav-item-inactive ember">
 				<a href="/people">People</a>
